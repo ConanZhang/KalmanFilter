a9f8f079dd01d3af3eb9a3d47f9e978b1f3448ff
ConanZhang <conankzhang@gmail.com>
2016-11-15 22:15:25 -0700

    Changed some data graphing values

--- CS4300_A6_data.m
+++ CS4300_A6_data.m
@@ -6,7 +6,7 @@ function data = CS4300_A6_data()
 %     Rajul Ramchandani & Conan Zhang
 %     UU
 %     Fall 2016
-[x_trace,a_trace,z_trace,Sigma2_trace] = CS4300_A6_driver_lin(0,0,1,1,1,0.1,pi/4);
+[x_trace,a_trace,z_trace,Sigma2_trace] = CS4300_A6_driver_lin(0,0,1,1,1,0.1,pi/8);
 plot(a_trace(:,1), a_trace(:,2), x_trace(:,1), x_trace(:,2));
 %set(gca,'XTick',t_list );
 %set(gca,'YTick', yticks);
@@ -15,8 +15,8 @@ plot(a_trace(:,1), a_trace(:,2), x_trace(:,1), x_trace(:,2));
 % ylabel('Size of KB')
 
 
-% [xt,at,zt,St] = CS4300_driver_proj(0.05,70,0.35,9.8,2,10);
-% plot(at(:,1), at(:,2), xt(:,1), xt(:,2));
+[xt,at,zt,St] = CS4300_driver_proj(0.05,70,0.35,9.8,2,10);
+plot(at(:,1), at(:,2), xt(:,1), xt(:,2));
 %set(gca,'XTick',t_list );
 %set(gca,'YTick', yticks);
 % title('Figure 1: Steps vs KB Size')
