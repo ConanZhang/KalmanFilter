2425cd848aa0d36c3ca22bed46e4a816a3b05bf7
ConanZhang <conankzhang@gmail.com>
2016-11-17 03:32:41 -0700

    All data collected

--- CS4300_A6_data.m
+++ CS4300_A6_data.m
@@ -57,20 +57,22 @@ function data = CS4300_A6_data()
  time = [];
  hold on
  [xt,at,zt,St] = CS4300_driver_proj(0.5, 20, 2,9.8,0.02,3);
- for t= [0.5:0.5:21]
-     time(end+1) = t;
- end
- plot(time, at(:,3), 'DisplayName', 'Actual vel in x');
- hold on
- plot(time, at(:,4),  'DisplayName', 'Actual vel in y');
- hold on
- plot(time, xt(:,3), 'DisplayName', 'Estimated vel in x');
- hold on
- plot(time, xt(:,4),  'DisplayName', 'Estimated vel in y');
+ 
+dif = abs(at(:,4) - xt(:,4));
+M = mean(dif);
+V = var(dif);
+CI1 = M - 1.96*sqrt(V/length(dif));
+CI2 = M + 1.96*sqrt(V/length(dif));
 
+%  for t= [0.5:0.5:21]
+%      time(end+1) = t;
+%  end
+%  plot(time, at(:,3), 'DisplayName', 'Actual vel in x');
+%  plot(time, at(:,4),  'DisplayName', 'Actual vel in y');
+%  plot(time, xt(:,3), 'DisplayName', 'Estimated vel in x');
+%  plot(time, xt(:,4),  'DisplayName', 'Estimated vel in y');
 % 
-% %plot(xt(:,3), xt(:,4), '.r', 'DisplayName', 'Estimated Trajectory');
-legend('show');
-xlabel('X')
-ylabel('Y')
+% legend('show');
+% xlabel('X')
+% ylabel('Y')
 
