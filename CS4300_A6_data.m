febf0ca36d7b5c3d3eb6dacd303bb2e12707453f
ConanZhang <conankzhang@gmail.com>
2016-11-16 17:23:43 -0700

    Added legend to linear graph

--- CS4300_A6_data.m
+++ CS4300_A6_data.m
@@ -7,17 +7,20 @@ function data = CS4300_A6_data()
 %     UU
 %     Fall 2016
 [x_trace,a_trace,z_trace,Sigma2_trace] = CS4300_A6_driver_lin(0,0,1,1,1,0.1,pi/8);
-plot(a_trace(:,1), a_trace(:,2), x_trace(:,1), x_trace(:,2), 'rx', z_trace(:,1), z_trace(:,2), 'go');
-hold
-plot(x_trace(:,1), x_trace(:,2), 'r');
-%set(gca,'XTick',t_list );
-%set(gca,'YTick', yticks);
+hold on
+plot(a_trace(:,1), a_trace(:,2), 'DisplayName', 'Actual');
+plot(x_trace(:,1), x_trace(:,2), 'r', 'DisplayName', 'Estimated (connected)');
+plot(x_trace(:,1), x_trace(:,2), 'rx', 'DisplayName', 'Estimated (points)');
+plot(z_trace(:,1), z_trace(:,2), 'go', 'DisplayName', 'Sensors');
+legend('show', 'Location', 'northwest');
 % title('Figure 1: Steps vs KB Size')
-% xlabel('No. of steps taken')
-% ylabel('Size of KB')
+xlabel('X(m)')
+ylabel('Y(m)')
 
-[xt,at,zt,St] = CS4300_driver_proj(0.05,70,0.35,9.8,0.008,18000);
-plot(at(:,1), at(:,2), xt(:,1), xt(:,2));
+hold off
+
+% [xt,at,zt,St] = CS4300_driver_proj(0.05,70,0.35,9.8,0.008,18000);
+% plot(at(:,1), at(:,2), xt(:,1), xt(:,2));
 %set(gca,'XTick',t_list );
 %set(gca,'YTick', yticks);
 % title('Figure 1: Steps vs KB Size')
